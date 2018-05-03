4793feb833b0c61f530fb41a88282a2b3af91eba
wangxl <wangxl3@mapbar.com>
2018-05-03 16:41:28 +0800

    add the class of RP way point

--- rp/RPRouteCalcRequest.hpp
+++ rp/RPRouteCalcRequest.hpp
@@ -1,6 +1,34 @@
 #pragma once
 #include <memory>
 
+class CPointCoord2D
+{
+public:
+	CPointCoord2D(int px = 0, int py = 0) : x(px), y(py) {}
+
+	int															x;
+	int															y;
+};
+
+enum RPWayPointType
+{
+	RPWayPointType_Coord,
+	RPWayPointType_RSLink,
+	RPWayPointType_Invalid
+};
+
+
+class CRPWayPoint : public CBaseObj
+{
+public:
+	CRPWayPoint();
+	CRPWayPoint(const CPointCoord2D &clCoord);
+public:
+	RPWayPointType												m_enWayPointType;
+	CPointCoord2D												m_clCoord;
+};
+
+
 class CRPWayPoints
 {
 public:
