490075d571ad59fef236dbf7933ed2a88cecc664
wangxl <wangxl3@mapbar.com>
2018-05-04 11:03:08 +0800

    assign start and end points

--- rp/RouteSession.cpp
+++ rp/RouteSession.cpp
@@ -33,10 +33,13 @@ void CRouteSession::calcRoute()
 {
 	auto spclWayPoints = std::make_shared<CRPWayPoints>();
 	spclWayPoints->m_vclWayPointList.emplace_back(CPointCoord2D(1389087203, 476456031));
+	spclWayPoints->m_vclWayPointList.emplace_back(CPointCoord2D(1389087203, 476456031));
 
 	CRPRouteCalcRequest		req(spclWayPoints);
 	
 	CRPRouteCalcRequest		clSectionRequest(req);
+	clSectionRequest.m_spclWayPoints->PushBack(spclWayPoints->At(0));
+	clSectionRequest.m_spclWayPoints->PushBack(spclWayPoints->At(1));
 
 	auto spclDPFacade = CDPFacade::Create();
 
