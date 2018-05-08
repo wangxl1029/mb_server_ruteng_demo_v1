9da3294f080a3e597d7b13b247632026506b45a4
wangxl <wangxl3@mapbar.com>
2018-05-08 10:45:05 +0800

    Correct the location of the missing projects of the solution.

---
+++ DataProvider/InnerCommon.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include	"ELErrLog.h"
+
+#include <math.h>
+
+#include	<vector>
+#include	<list>
+#include	<map>
+#include	<set>
+#include	<stack>
+#include	<deque>
+#include	<queue>
+#include	<string>
+#include	<algorithm>
+using namespace std;
+
+#include	"nds_sqlite3.h"
+#ifdef WXL_CLIENT_SPECIFIED
+#include	"IOLibCommon_HeaderAll.h"
+#include	"IOLibNDS_HeaderAll.h"
+
+#include	"CFHeaderAll.h"
+#endif
+#include	"SL_HeaderAll.h"
+
+#define		NEU_DB
+
